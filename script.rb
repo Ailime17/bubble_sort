d89609f1f47c5e7c9504c3ce7ecc514744a7524a
Emilia <emilia.skrzypczak.es@gmail.com>
2022-11-14 10:48:36 +0100

    Add break statement to improve efficiency

--- script.rb
+++ script.rb
@@ -1,16 +1,18 @@
 def bubble_sort(array)
-
-    (array.length - 1).times do
-      array.each_with_index do |number,index|
-        if index+1 < array.length
-          if array[index] > array[index+1]
-            array[index], array[index+1] = array[index+1], array[index]
-          end
+  (array.length - 1).times do
+    n = 0
+    array.each_with_index do |_val, index|
+      if index+1 < array.length
+        if array[index] > array[index+1]
+          array[index], array[index+1] = array[index+1], array[index]
+        else
+          n += 1
         end
       end
     end
-    p array
-    return array
-
+    break if n == array.length-1
   end
-  bubble_sort([4,3,78,2,0,2,9])
+  p array
+  return array
+end
+bubble_sort([3,2,6,9,1,-1])
